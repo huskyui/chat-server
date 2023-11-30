d61f618be7e42b23f365f679e3a9f51038a393bb
huskyui <2207019991@qq.com>
2023-11-30 08:58:54 +0800

    feat:websocket连接后返回handShake信息

--- hub.go
+++ hub.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "encoding/json"
 
 type Hub struct {
 	clients    map[*client]bool
@@ -9,18 +9,29 @@ type Hub struct {
 	unregister chan *client
 }
 
-var hub = &Hub{clients: make(map[*client]bool), broadcast: make(chan []byte), register: make(chan *client), unregister: make(chan *client)}
+var hub = &Hub{
+	clients:    make(map[*client]bool),
+	broadcast:  make(chan []byte),
+	register:   make(chan *client),
+	unregister: make(chan *client),
+}
 
 func (h *Hub) run() {
 	for {
 		select {
 		case client := <-h.register:
 			h.clients[client] = true
-			fmt.Println("newClient", client)
+			sendHandShake(client)
 		case bytes := <-h.broadcast:
 			for client := range h.clients {
 				client.send <- bytes
 			}
 		}
 	}
+}
+
+func sendHandShake(c *client) {
+	message := Message{Type: "handshake", Content: "hello from server"}
+	bytes, _ := json.Marshal(message)
+	c.send <- bytes
 }
---
+++ message.go
@@ -0,0 +1,7 @@
+package main
+
+type Message struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+	User    string `json:"user"`
+}
